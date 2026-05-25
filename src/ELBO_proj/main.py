256d0f4f90a9d7e5e6bf5a87f5059d8064123e12
root <root@localhost.localdomain>
2026-05-25 19:53:52 +0000

    20260526 current parameter set and checkpoint update need more modification

--- src/ELBO_proj/main.py
+++ src/ELBO_proj/main.py
@@ -16,8 +16,8 @@ def main():
     z_dim = 48
     h_dim = 48
     embed_dim = 32
-    para_mu = 1000.0
-    para_lambda = 1.2
+    para_mu = 10.0
+    para_lambda = 0.0001
 
     _, U_seq_dim = trainData.U_seq.shape
     if (U_seq_dim - x_dim) % u_dim != 0:
