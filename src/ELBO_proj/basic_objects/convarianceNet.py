1ef08b6bdfae2a6a4f6e705774e501d50a33c7c9
kayee_yuen <2676904949@qq.com>
2026-05-25 15:33:13 +0800

    20260525 Sigma_00 in cov is now a diag

--- src/ELBO_proj/basic_objects/convarianceNet.py
+++ src/ELBO_proj/basic_objects/convarianceNet.py
@@ -3,16 +3,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 """_功能介绍_
-    这是一个通过指定输入 u0 (或者 I_hat_0) 来构造潜变量协方差矩阵 Sigma_00 的类
-    协方差矩阵 Sigma_00 通过对下三角矩阵 L 执行 L @ L.T 得到
+    这是一个通过指定输入 u0 或者 I_hat_0 来构造潜变量初始协方差矩阵 Sigma_00 的类。
+    当前版本将 Sigma_00 构造为对角矩阵，其主对角线元素由 nn.Sequential 网络输出。
+    为了保证协方差矩阵半正定，网络输出会经过 softplus 变换，使对角元素为正数。
+
     Inputs:
-            u0.shape = [1, u_dim] (或者可以输入多个 batch 的 u_dim)
-            ** 注意, 针对 u0.shape = u_dim 的情况，可以在外部使用
-                u0 = u0.unsqueeze(0) 对 u_0 的维度进行扩展，然后再使用 CovarianceNet(u0)
+            u0.shape = [dim_u]
+            或者
+            u0.shape = [batch, dim_u]
+
     Returns:
-            Sigma_00.shape = [batch, dim_z, dim_z]
-            ** 注意, 如果只需要 shape = [dim_z, dim_z], 可以在外部使用
-                Sigma_00 = Sigma_00[0]  # [dim_z, dim_z]
+            如果输入 u0.shape = [dim_u]：
+                Sigma_00.shape = [dim_z, dim_z]
+            如果输入 u0.shape = [batch, dim_u]：
+                Sigma_00.shape = [batch, dim_z, dim_z]
 """
 
 
@@ -22,13 +26,13 @@ class initCovarianceNet(nn.Module):
 
         self.dim_z = dim_z
 
-        # 输出 dim_z * dim_z 个数，用来组成 raw_L
+        # 输出 dim_z 个数，用来组成 Sigma_00 的主对角线元素。
         self.net = nn.Sequential(
             nn.Linear(dim_u, hidden_dim),
             nn.Tanh(),
             nn.Linear(hidden_dim, hidden_dim),
             nn.Tanh(),
-            nn.Linear(hidden_dim, dim_z * dim_z),
+            nn.Linear(hidden_dim, dim_z),
         )
 
     def forward(self, u0):
@@ -54,22 +58,13 @@ class initCovarianceNet(nn.Module):
 
         batch_size = u0.shape[0]
 
-        raw_L = self.net(u0)  # [batch, dim_z * dim_z]
-        raw_L = raw_L.reshape(batch_size, self.dim_z, self.dim_z)
+        raw_diag = self.net(u0)  # [batch, dim_z]
 
-        # 取下三角部分
-        L = torch.tril(raw_L)
+        # 保证协方差矩阵的主对角线元素为正数。
+        diag_positive = F.softplus(raw_diag) + 1e-3
 
-        # 保证对角线为正数
-        diag = torch.diagonal(L, dim1=-2, dim2=-1)
-        diag_positive = F.softplus(diag) + 1e-6
-
-        L = L.clone()
-        idx = torch.arange(self.dim_z, device=u0.device)
-        L[:, idx, idx] = diag_positive
-
-        # 构造协方差矩阵
-        Sigma_00 = L @ L.transpose(-1, -2)
+        # 构造对角协方差矩阵。
+        Sigma_00 = torch.diag_embed(diag_positive)  # [batch, dim_z, dim_z]
 
         if single_input:
             Sigma_00 = Sigma_00.squeeze(0)
